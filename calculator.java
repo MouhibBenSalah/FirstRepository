e48790a6fbc3af45b1d5ca865bb8de2eb868144a
mouhib <mouhib.bensalah.bsm12@gmail.com>
2024-03-01 12:54:31 +0100

    First Repository

---
+++ calculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Calculatrice {
+    public static double addition(double a, double b) {
+        return a + b;
+    }
+
+    public static double soustraction(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public static double division(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division par zéro impossible");
+        }
+        return a / b;
+    }
+}
+
+
+
